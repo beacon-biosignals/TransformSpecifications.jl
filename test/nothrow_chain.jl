e6400363c3ad43a3db84ff01e7449245e991f93f
hannahilea <hannahilea@users.noreply.github.com>
2023-08-04 17:13:27 -0400

    wip

--- test/nothrow_chain.jl
+++ test/nothrow_chain.jl
@@ -21,58 +21,6 @@ end
     str::String
 end
 
-@testset "Basic `NoThrowTransformChain`" begin
-    using TransformSpecifications: input_assembler
-
-    steps = [ChainStep("init", nothing,
-                       NoThrowTransform(SchemaFooV1, SchemaBarV1,
-                                        x -> SchemaBarV1(; var1=x.foo * "_a",
-                                                         var2=x.foo * "_a2"))),
-             ChainStep("middle",
-                       input_assembler(d -> (; foo=d["init"][:var1])),
-                       NoThrowTransform(SchemaFooV1, SchemaFooV1,
-                                        x -> SchemaFooV1(; foo=x.foo * "_b"))),
-             ChainStep("final",
-                       input_assembler(d -> (; var1=d["init"][:var2],
-                                             var2=d["middle"][:foo])),
-                       NoThrowTransform(SchemaBarV1, SchemaFooV1,
-                                        x -> SchemaFooV1(;
-                                                         foo=string(x.var1, "_WOW_",
-                                                                    x.var2))))]
-    chain = NoThrowTransformChain(steps)
-    @test chain isa NoThrowTransformChain
-
-    @testset "Internals" begin
-        @test issetequal(keys(chain.step_input_assemblers), keys(chain.step_transforms))
-        @test issetequal(keys(chain._step_output_fields), keys(chain.step_transforms))
-        @test length(steps) == length(chain) == 3
-        @test isequal(steps, map(i -> get_step(chain, i), 1:length(chain)))
-        @test isequal(steps, map(n -> get_step(chain, n), [s.name for s in steps]))
-
-        @test_throws KeyError get_step(chain, "nonexistent_step")
-        @test_throws BoundsError get_step(chain, 15)
-    end
-
-    @testset "Externals" begin
-        @test chain isa AbstractTransformSpecification
-        @test input_specification(chain) == SchemaFooV1
-        @test output_specification(chain) == NoThrowResult{SchemaFooV1}
-    end
-
-    @testset "Conforming input succeeds" begin
-        input_record = SchemaFooV1(; foo="rabbit")
-        chain_output = transform!(chain, input_record)
-        @test nothrow_succeeded(chain_output)
-        @test chain_output isa NoThrowResult{SchemaFooV1}
-        @test chain_output.result.foo == "rabbit_a2_WOW_rabbit_a_b"
-
-        conforming_input_record = SchemaFooChildV1(; foo="rabbit")
-        @test !(conforming_input_record isa input_specification(chain))
-        chain_output2 = transform!(chain, conforming_input_record)
-        @test isequal(chain_output, chain_output2)
-    end
-end
-
 @testset "Basics" begin
     step_a = ChainStep("init", nothing,
                        TransformSpecification(SchemaFooV1, SchemaFooV1, identity))
@@ -125,7 +73,75 @@ end
     end
 end
 
-@testset "Field maps" begin
+
+@testset "Basic `NoThrowTransformChain`" begin
+    using TransformSpecifications: input_assembler
+
+    steps = [ChainStep("init", nothing,
+                       NoThrowTransform(SchemaFooV1, SchemaBarV1,
+                                        x -> SchemaBarV1(; var1=x.foo * "_a",
+                                                         var2=x.foo * "_a2"))),
+             ChainStep("middle",
+                       input_assembler(d -> (; foo=d["init"][:var1])),
+                       NoThrowTransform(SchemaFooV1, SchemaFooV1,
+                                        x -> SchemaFooV1(; foo=x.foo * "_b"))),
+             ChainStep("final",
+                       input_assembler(d -> (; var1=d["init"][:var2],
+                                             var2=d["middle"][:foo])),
+                       NoThrowTransform(SchemaBarV1, SchemaFooV1,
+                                        x -> SchemaFooV1(;
+                                                         foo=string(x.var1, "_WOW_",
+                                                                    x.var2))))]
+    chain = NoThrowTransformChain(steps)
+    @test chain isa NoThrowTransformChain
+
+    @testset "Internals" begin
+        @test issetequal(keys(chain.step_input_assemblers), keys(chain.step_transforms))
+        @test issetequal(keys(chain._step_output_fields), keys(chain.step_transforms))
+        @test length(steps) == length(chain) == 3
+        @test isequal(steps, map(i -> get_step(chain, i), 1:length(chain)))
+        @test isequal(steps, map(n -> get_step(chain, n), [s.name for s in steps]))
+
+        @test_throws KeyError get_step(chain, "nonexistent_step")
+        @test_throws BoundsError get_step(chain, 15)
+    end
+
+    @testset "Externals" begin
+        @test chain isa AbstractTransformSpecification
+        @test input_specification(chain) == SchemaFooV1
+        @test output_specification(chain) == NoThrowResult{SchemaFooV1}
+    end
+
+    @testset "Conforming input succeeds" begin
+        input_record = SchemaFooV1(; foo="rabbit")
+        chain_output = transform!(chain, input_record)
+        @test nothrow_succeeded(chain_output)
+        @test chain_output isa NoThrowResult{SchemaFooV1}
+        @test chain_output.result.foo == "rabbit_a2_WOW_rabbit_a_b"
+
+        conforming_input_record = SchemaFooChildV1(; foo="rabbit")
+        @test !(conforming_input_record isa input_specification(chain))
+        chain_output2 = transform!(chain, conforming_input_record)
+        @test isequal(chain_output, chain_output2)
+    end
+
+
+    @testset "Failures handled" begin
+        _check_violation = (result, err_str) -> begin
+            @test !nothrow_succeeded(result)
+            @test isequal(err_str, only(result.violations))
+        end
+
+        let
+            #TODO-HERE! This test is failing---update error message to be as expected,
+            # then fix test! :)
+            result = transform!(chain, SchemaBarV1(; var1="yay", var2="whee"))
+            _check_violation(result, "")
+    end
+
+end
+
+@testset "Helper utilities" begin
     using TransformSpecifications: _field_map, construct_field_map
     @testset "`_field_map`" begin
         @test _field_map(String) == _field_map(NoThrowResult{String}) ==String
