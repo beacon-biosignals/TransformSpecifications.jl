460623dee02493d0884259c847e9efc0e8da0126
hannahilea <hannahilea@users.noreply.github.com>
2023-08-16 11:29:28 -0400

    add more documentation

--- src/mermaid.jl
+++ src/mermaid.jl
@@ -13,16 +13,26 @@ Future features:
 - clean up themeing: https://mermaid.js.org/config/theming.html
 =#
 
-const DEFAULT_OUTER_STYLE = "fill:#cbd7e2,stroke:#000,stroke-width:0px;"
-const DEFAULT_STEP_STYLE = "fill:#eeedff,stroke:#000,stroke-width:2px;"
-const DEFAULT_SPEC_STYLE = "fill:#f8f7ff,stroke:#000,stroke-width:1px;"
-const DEFAULT_SPEC_FIELD_STYLE = "fill:#fff,stroke:#000,stroke-width:1px;"
-
 """
-    mermaidify(dag::NoThrowDAG; direction="TB")
+    mermaidify(dag::NoThrowDAG; direction="LR",
+               style_step="fill:#eeedff,stroke:#000,stroke-width:2px;",
+               style_spec="fill:#f8f7ff,stroke:#000,stroke-width:1px;",
+               style_outer="fill:#cbd7e2,stroke:#000,stroke-width:0px;",
+               style_spec_field="fill:#fff,stroke:#000,stroke-width:1px;")
 
 Generate [mermaid plot](https://mermaid.js.org/) of `dag`, suitable for inclusion
 in markdown documentation.
+
+Args:
+* `direction`: option that specifies the orientation/flow of the dag's steps;
+    most useful options for dag plotting are `LR` (left to right) or `TB` (top to bottom);
+    see the mermaid documentation for full list of options.
+* `style_step`: styling of the box containing an individual dag step (node)
+* `style_spec`: styling of the boxes containing the input and output specifications for each step
+* `style_outer`: styling of the box bounding the entire DAG
+* `style_spec_field`: styling of the boxes bounding each specification's individual field(s)
+
+For each style kwarg, see the mermaid documentation for style string options.
 
 To include in markdown, do
 ````markdown
@@ -30,7 +40,7 @@ To include in markdown, do
 {{mermaidify output}}
 ```
 ````
-and for html (i.e., for Documenter.jl), do
+or for html (i.e., for Documenter.jl), do
 ````markdown
 <div class=\"mermaid\">
 {{mermaidify output}}
@@ -40,17 +50,21 @@ For an example of the raw output, see [`NoThrowDAG`](@ref); for an example
 of the rendered output, see [the built documentation](https://beacon-biosignals.github.io/TransformSpecifications.jl/dev).
 """
 function mermaidify(dag::NoThrowDAG; direction="LR",
-                    style_step=DEFAULT_STEP_STYLE,
-                    style_spec=DEFAULT_SPEC_STYLE, style_outer=DEFAULT_OUTER_STYLE,
-                    style_spec_field=DEFAULT_SPEC_FIELD_STYLE)
+                    style_step="fill:#eeedff,stroke:#000,stroke-width:2px;",
+                    style_spec="fill:#f8f7ff,stroke:#000,stroke-width:1px;",
+                    style_outer="fill:#cbd7e2,stroke:#000,stroke-width:0px;",
+                    style_spec_field="fill:#fff,stroke:#000,stroke-width:1px;")
     mermaid_lines = ["flowchart"]
 
+    # Add a box for each node (step) in the dag; the box contains descriptions of the input specification,
+    # output specification, and applied transformation function
     push!(mermaid_lines, "", "%% Define steps (nodes)")
     push!(mermaid_lines, """subgraph OUTERLEVEL["` `"]""", "direction $direction")
     for step in dag
         Base.append!(mermaid_lines, _mermaid_subgraph_from_dag_step(step))
     end
 
+    # Draw a connecting edge between each step in the dag
     push!(mermaid_lines, "", "%% Link steps (edges)")
     keys_upper = map(_mermaid_key, collect(keys(dag)))
     for i_key in 2:length(keys_upper)
@@ -58,8 +72,13 @@ function mermaidify(dag::NoThrowDAG; direction="LR",
         push!(mermaid_lines,
               "$(keys_upper[i_key - 1]):::classStep $arrow $(keys_upper[i_key]):::classStep")
     end
+
+    # This additional edge is hidden ("~~~") but requried for the outer level to
+    # be drawn in the desired orientation (it could potentially be dropped in the future,
+    # as long as the plot shows up okay without it! Was added via trial and error...)
     push!(mermaid_lines, "", "end", "OUTERLEVEL:::classOuter ~~~ OUTERLEVEL:::classOuter")
 
+    # Define the styles applied to the assorted node boxes
     push!(mermaid_lines, "", "%% Styling definitions")
     for (name, style) in
         [("classOuter", style_outer), ("classStep", style_step), ("classSpec", style_spec),
@@ -86,6 +105,8 @@ function _mermaid_subgraph_from_dag_step(step::DAGStep)
     process = step.transform_spec
     node_key = _mermaid_key(key)
 
+    # Helper function that adds a "specification" box (graph node), which lists the internal
+    # fields of the specification
     _schema_subgraph = (fieldmap::Dict, prefix) -> begin
         content = map(collect(keys(fieldmap))) do fieldname
             type = fieldmap[fieldname]
@@ -96,13 +117,15 @@ function _mermaid_subgraph_from_dag_step(step::DAGStep)
         return collect(Iterators.flatten(content))
     end
 
+    # Add a specification box for input spec...
     inputs_subgraph = let
         prefix = "_InputSchema"
-        contents = _schema_subgraph(field_dict(input_specification(process)),
-                                    prefix)
+        contents = _schema_subgraph(field_dict(input_specification(process)), prefix)
         label = string("Input: ", input_specification(process))
         _mermaid_subgraph(node_key * prefix, label; contents, direction="RL")
     end
+
+    # ...and output spec...
     outputs_subgraph = let
         prefix = "_OutputSchema"
         type = result_type(output_specification(process))
@@ -111,6 +134,7 @@ function _mermaid_subgraph_from_dag_step(step::DAGStep)
         _mermaid_subgraph(node_key * prefix, label; contents, direction="RL")
     end
 
+    # ...and add an arrow (edge) between them, that is labeled with the dag step's transform function
     node_contents = reduce(vcat,
                            [inputs_subgraph, outputs_subgraph,
                             "$(node_key)_InputSchema:::classSpec -- $(process.transform_spec.transform_fn) --> $(node_key)_OutputSchema:::classSpec"])
